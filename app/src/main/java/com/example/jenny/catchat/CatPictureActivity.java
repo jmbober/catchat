124a04a73b2d0dbd17cb84e0c2134bdea98e0bd6
Jenny Bober <jmbober@svsu.edu>
2017-10-25 16:11:35 -0400

    Submitting project

---
+++ app/src/main/java/com/example/jenny/catchat/CatPictureActivity.java
@@ -0,0 +1,13 @@
+package com.example.jenny.catchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatPictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_picture);
+    }
+}
